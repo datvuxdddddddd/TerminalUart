8ac0e0462b66edc191c4c6e467bd2826c2cf33cc
GAMING PC <dat.vu.xdddddddd@hcmut.edu.vn>
2020-05-15 10:20:56 +0700

    UI Complete

---
+++ app/src/main/java/uart/terminal/androidstudio/com/myapplication/Values.java
@@ -0,0 +1,8 @@
+package uart.terminal.androidstudio.com.myapplication;
+
+public class Values {
+    public static int baudrate;
+    public static int dataBits;
+    public static String parity;
+    public static int stopBits;
+}
